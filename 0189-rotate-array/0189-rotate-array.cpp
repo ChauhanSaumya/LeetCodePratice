49695f0a558e5accd1d3791dedfedbab4f50949b
Saumya Chauhan <155440810+ChauhanSaumya@users.noreply.github.com>
2025-08-05 14:40:44 +0530

    Time: 0 ms (100%), Space: 29.4 MB (97.7%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -3,9 +3,8 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        if(k==0)return;
+        reverse(nums.begin(),nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k),nums.end());
         reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
     }
 };
