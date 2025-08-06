be15e85161586d035eda5d4e2b192619e3c40732
Saumya Chauhan <155440810+ChauhanSaumya@users.noreply.github.com>
2025-08-06 23:51:14 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.95%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,39 +1,29 @@
-#include <vector>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
-    vector<int> nextPermutation(vector<int>& nums) {
-        int ind = -1;
-        int n = nums.size();
-
-        // Step 1: Find the largest index `ind` such that nums[ind] < nums[ind + 1]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                ind = i; // Store the index, not the value
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int ind=-1;
+        for(int i=n-2;i>=0;i--)
+        {
+            if(nums[i+1]>nums[i])
+            {
+                ind = i;
+                break;
+            }
+        }
+        if(ind == -1) {
+             reverse(nums.begin(), nums.end());
+             return;
+        }
+         for(int i=n-1;i>ind;i--)
+        {
+            if(nums[i]>nums[ind])
+            {
+                swap(nums[i],nums[ind]);
                 break;
             }
         }
 
-        // If no such index exists, the array is in descending order
-        if (ind == -1) {
-            reverse(nums.begin(), nums.end());
-            return nums;
-        }
-
-        // Step 2: Find the largest index `j` greater than `ind` such that nums[ind] < nums[j]
-        for (int j = n - 1; j > ind; j--) {
-            if (nums[j] > nums[ind]) {
-                swap(nums[ind], nums[j]); // Swap the values at indices `ind` and `j`
-                break;
-            }
-        }
-
-        // Step 3: Reverse the sequence from `ind + 1` to the end of the array
-        reverse(nums.begin() + ind + 1, nums.end());
-
-        return nums;
+        reverse(nums.begin()+ind+1,nums.end());
     }
 };
